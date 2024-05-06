a29d8b93315630999a37f5bdd05db1bd2b604353
maxberends <maxberends001@gmail.com>
2024-05-06 20:33:58 +0200

    Update and rename esp32bnonow.ino to ESP_NOW_receiver_BNO.ino

---
+++ ESP32/ESP_NOW_receiver_BNO.ino
@@ -0,0 +1,108 @@
+/*
+06052024 werkend met TTGO lora32 oled
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  char a[6];
+  float b;
+  float c;
+  float d;
+  float e;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Char: ");
+  Serial.println(myData.a);
+  Serial.print("Int: ");
+  Serial.println(myData.b);
+  Serial.print("Float: ");
+  Serial.println(myData.c);
+  Serial.print("Float2: ");
+  Serial.println(myData.d);
+  Serial.println();
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.print("Char: ");
+  display.println(myData.a);
+  display.print("X: ");
+  display.println(myData.b);
+  display.print("Y: ");
+  display.println(myData.c);
+  display.print("GYRO x: ");
+  display.println(myData.d);
+  display.print("ACCEL x: ");
+  display.println(myData.e);
+  display.display(); 
+
+  
+
+}
+ 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
+  
+  delay(2000);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+
+
+
+
+}
+ 
+void loop() {
+
+}
